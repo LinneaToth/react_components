b787685abcda60b562d367603e37bbc13993d473
Linnéa Toth <linnea@liroit.com>
2025-05-20 14:50:16 +0200

    path to img

--- src/components/sidebar/Figure.jsx
+++ src/components/sidebar/Figure.jsx
@@ -1,10 +1,12 @@
+import figure from "../../../src/assets/img/figure.png";
+
 //Just an image to fill out the emptiness..
 export default function Figure() {
   return (
     <img
       alt="abstract illustration"
-      src="../../../src/assets/img/figure.png"
-      className="w-[175px] h-[175px] rounded-[50%] self-center justify-self-center grid-row-2 row-span-2 mt-45
-      "></img>
+      src={figure}
+      className="grid-row-2 row-span-2 mt-45 h-[175px] w-[175px] self-center justify-self-center rounded-[50%]"
+    ></img>
   );
 }
